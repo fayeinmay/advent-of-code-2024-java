46eb9dfc9b1925fe6445ba99131788e41f70b021
Faye Schipper <business@fayedev.de>
2024-12-07 01:58:57 +0100

    Day 4!

--- src/com/fayeinmay/adventofcode/Main.java
+++ src/com/fayeinmay/adventofcode/Main.java
@@ -3,6 +3,6 @@ package com.fayeinmay.adventofcode;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("The solution for today's puzzle is: " + Day3.getSolution());
+        System.out.println("The solution for today's puzzle is: " + Day4.getSolution());
     }
 }
